df4b5a5651b6ac739c420a0337be88ec4ad279d9
Misael Saenz Flores <misa2pac@gmail.com>
2016-11-29 11:44:51 -0600

    Se agregaron nuevos archivos
    Se agrego main.c y main.h

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+// No hagas #include aquí, hazlos en main.h
+
+int main(int argc, char const *argv[])
+{
+    // Verifica si hay al menos de 3 parametros
+    // sino entonces se imprimen las instrucciones
+    // y se para la ejecución del programa
+    if (argc < 3)
+    {
+        imprime_instrucciones();
+        return 0;
+    }
+
+    return 0;
+}
+
+/* ------ Aquí va la implementación de tus funciones ------ */
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H // Guarda para evitar include circulares
+#define MAIN_H
+/* ------ Se hacen todos los includes necesarios ------ */
+
+/* ------ Definición de tus estructuras ------ */
+
+/* ------ Definición de tus funciones ------ */
+
+/*Imprime las instrucciones de uso del programa*/
+void imprime_instrucciones();
+
+#endif //MAIN_H
